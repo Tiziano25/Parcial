6fedb12d78c709400b35ef385bc27a38bfc5ce96
Tiziano25 <tizianocortalezzi53@gmail.com>
2025-06-26 18:58:13 -0300

    Punto 2 completado

--- main.c
+++ main.c
@@ -14,13 +14,23 @@ int main(int argc, char const *argv[])
     uint32_t cant;
     llenar_datos(&header, &data, &cant, f);
 
+    //Punto 1
     printf("Nombres de las canciones:\n");
-    for (uint32_t i = 0; i < cant; i++)
+    for (int i = 0; i < cant; i++)
     {
         printf("Cancion %u: %s\n", (i+1), header[i].sound_name);
     }
     
+    //Punto 2
+    printf("\nEliga que cancion desea escuchar (El numero a elegir debe ser entre 1 y 4): ");
+    int n;
+    do
+    {
+        scanf("%d", &n);
+    } while (n>4||n<1);
+    play_audio(header[n-1].sample_rate, header[n-1].sample_count, data[n-1].sound_data);
 
+    //Liberacion de memoria
     for (uint32_t i = 0; i < cant; i++)
     {
         free(data[i].sound_data);
